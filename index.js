15e55697564aec7bc6cf67be0cf6d0b7a10983d3
Ashwin <146985701+Ashwin15042@users.noreply.github.com>
2026-03-16 16:56:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,19 @@ const cors = require("cors");
 
 const app = express();
 
-// 🔥 Middleware FIRST
-
 app.use(cors());
 app.use(express.json());
 
-// Routes
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-// MongoDB Connection
 mongoose.connect("mongodb://127.0.0.1:27017/taskmanager")
   .then(() => console.log("MongoDB Connected Successfully"))
   .catch((err) => console.log(err));
 
-// Start Server
 app.listen(5000, () => {
   console.log("Server started on port 5000");
 });
